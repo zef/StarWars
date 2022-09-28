6e7a863402b4eaf8aec60a005fec2cd1d26460d5
Zef Houssney <zef@zef.studio>
2022-09-28 13:44:28 -0600

    Initial commit.

---
+++ StarWars/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StarWars
+//
+//  Created by Zef Houssney on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, \(Person.luke.name)!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWars/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  StarWars
+//
+//  Created by Zef Houssney on 9/28/22.
+//
+
+import Foundation
+
+struct Person {
+    var name: String
+
+    static var luke: Person {
+        Person(name: "Luke Skywalker")
+    }
+//    var height: Int
+//    var mass: Int
+}
+
