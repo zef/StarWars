d3f63835f0ff40965c7e1ce8481a06ff55d5ba44
Zef Houssney <zef@zef.studio>
2022-10-17 09:54:12 -0600

    ContentView -> PeopleView

--- StarWars/PeopleView.swift
+++ StarWars/PeopleView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  PeopleView.swift
 //  StarWars
 //
 //  Created by Zef Houssney on 9/28/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PeopleView: View {
 
     @State var people = [Person]()
 
@@ -33,8 +33,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct PeopleView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        PeopleView()
     }
 }
--- StarWars/StarWarsApp.swift
+++ StarWars/StarWarsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StarWarsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PeopleView()
         }
     }
 }
