0e497e35e6bc57c87453519af6ea0041b54891eb
Zef Houssney <zef@zef.studio>
2022-10-17 09:53:08 -0600

    Add some new variables to Person, make it Identifiable so we can use it in a SwiftUI List.

--- StarWars/Person.swift
+++ StarWars/Person.swift
@@ -6,11 +6,25 @@
 //
 
 import Foundation
-
 struct Person: Codable {
     var name: String
-//    var height: S?
-//    var mass: Int?
+    var height: Int?
+    var mass: Int?
+    var hairColor: String
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+
+        if let heightString = try container.decodeIfPresent(String.self, forKey: .height) {
+            self.height = Int(heightString)
+        }
+        if let massString = try container.decodeIfPresent(String.self, forKey: .mass) {
+            self.mass = Int(massString)
+        }
+        self.hairColor = try container.decode(String.self, forKey: .hairColor)
+    }
+
 
     static var luke: Person? {
         Person.fromJSONFile(named: "luke")
@@ -21,5 +35,11 @@ struct Person: Codable {
             return try? Person.decodeJSON(from: data)
         }
         return nil
+    }
+}
+
+extension Person: Identifiable {
+    var id: String {
+        name
     }
 }
