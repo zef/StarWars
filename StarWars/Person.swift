17e098121c56e83a46a0a7fd93caed108be86df7
Zef Houssney <zef@zef.studio>
2022-10-17 09:51:55 -0600

    Refactor person to use decoding extensions from API.

--- StarWars/Person.swift
+++ StarWars/Person.swift
@@ -16,20 +16,9 @@ struct Person: Codable {
         Person.fromJSONFile(named: "luke")
     }
 
-    static func fromJSON(data: Data) -> Person? {
-        let decoder = JSONDecoder()
-        do {
-            let luke = try decoder.decode(Person.self, from: data)
-            return luke
-        } catch {
-            print("Could not make person from data.", error.localizedDescription)
-            return nil
-        }
-    }
-
     static func fromJSONFile(named name: String) -> Person? {
         if let data = Data.fromJSONFile(forName: name) {
-            return fromJSON(data: data)
+            return try? Person.decodeJSON(from: data)
         }
         return nil
     }
